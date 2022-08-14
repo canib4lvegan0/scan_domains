a90f9b97265d22d811db06030ac27540ead6b52b
Robson Sousa <sousa.robson68@gmail.com>
2022-08-14 14:43:17 -0300

    atualizado

--- app.py
+++ app.py
@@ -1,19 +1,71 @@
 import os
 from typing import List
-
+import time
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from requests import get
-
+from threading import Thread
 from names import names
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 
 db = SQLAlchemy()
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+
+def _scanner_1(name):
+    print("running scanner_3 for {}".format(name))
+    if not RegisterModel.find_by_name(name):
+        url = f"http://www.{name}.com"
+        try:
+            resp = get(url=url)
+            if resp.status_code == 200:
+                print(f"{url} - ok")
+                RegisterModel(name=name, url=url).save_to_db()
+        except:
+            pass
+
+def _scanner_2(name):
+    print("running scanner_2 for {}".format(name))
+    if not RegisterModel.find_by_name(name):
+        url = f"http://www.{name}.com"
+        try:
+            resp = get(url=url)
+            if resp.status_code == 200:
+                print(f"{url} - ok")
+                RegisterModel(name=name, url=url).save_to_db()
+        except:
+            pass
+
+def _scanner_3(name):
+    print("running scanner_3 for {}".format(name))
+    if not RegisterModel.find_by_name(name):
+        url = f"http://www.{name}.com"
+        try:
+            resp = get(url=url)
+            if resp.status_code == 200:
+                print(f"{url} - ok")
+                RegisterModel(name=name, url=url).save_to_db()
+        except:
+            pass
+
+def _scanner_4(name):
+    print("running scanner_3 for {}".format(name))
+    if not RegisterModel.find_by_name(name):
+        url = f"http://www.{name}.com"
+        try:
+            resp = get(url=url)
+            if resp.status_code == 200:
+                print(f"{url} - ok")
+                RegisterModel(name=name, url=url).save_to_db()
+        except:
+            pass
 
 
 class RegisterModel(db.Model):
@@ -43,14 +95,6 @@ class RegisterModel(db.Model):
         }
 
 
-def _try_get(url):
-    try:
-        resp = get(url=url)
-        return resp.status_code == 200
-    except:
-        pass
-
-
 @app.before_first_request
 def create_tables():
     db.create_all()
@@ -58,11 +102,17 @@ def create_tables():
 
 @app.route('/scan')
 def scan_urls():
+
     for name in names:
-        url = f"http://www.{name}.com"
-        if _try_get(url):
-            if not RegisterModel.find_by_name(name):
-                RegisterModel(name=name, url=url).save_to_db()
+        if name[0] in "abcde":
+            Thread(target=_scanner_1(name), args=[]).start()
+        if name[0] in "fghij":
+            Thread(target=_scanner_2(name), args=[]).start()
+        if name[0] in "klmnop":
+            Thread(target=_scanner_3(name), args=[]).start()
+        if name[0] in "qrstuvwxyz":
+            Thread(target=_scanner_4(name), args=[]).start()
+
     return redirect(url_for('get_urls'))
 
 
@@ -71,8 +121,6 @@ def get_urls():
     return {"urls": [r.to_json() for r in RegisterModel.get_urls()]}, 200
 
 db.init_app(app)
-# app.run()
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run()
