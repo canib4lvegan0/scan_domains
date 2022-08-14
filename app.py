2b3577303d3f293cca21f336830490a933039d7d
Robson Sousa <sousa.robson68@gmail.com>
2022-08-14 19:00:16 -0300

    finish

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import os
 from typing import List
-import time
+from requests import get
+from threading import Thread
+
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-from requests import get
-from threading import Thread
-from names import names
-from dotenv import load_dotenv
+from sqlalchemy import text
 
+from dotenv import load_dotenv
 load_dotenv(".env")
+
+from names import names, clean_names
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -19,58 +21,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 
-def _scanner_1(name):
-    print("running scanner_3 for {}".format(name))
-    if not RegisterModel.find_by_name(name):
-        url = f"http://www.{name}.com"
-        try:
-            resp = get(url=url)
-            if resp.status_code == 200:
-                print(f"{url} - ok")
-                RegisterModel(name=name, url=url).save_to_db()
-        except:
-            pass
-
-def _scanner_2(name):
-    print("running scanner_2 for {}".format(name))
-    if not RegisterModel.find_by_name(name):
-        url = f"http://www.{name}.com"
-        try:
-            resp = get(url=url)
-            if resp.status_code == 200:
-                print(f"{url} - ok")
-                RegisterModel(name=name, url=url).save_to_db()
-        except:
-            pass
-
-def _scanner_3(name):
-    print("running scanner_3 for {}".format(name))
-    if not RegisterModel.find_by_name(name):
-        url = f"http://www.{name}.com"
-        try:
-            resp = get(url=url)
-            if resp.status_code == 200:
-                print(f"{url} - ok")
-                RegisterModel(name=name, url=url).save_to_db()
-        except:
-            pass
-
-def _scanner_4(name):
-    print("running scanner_3 for {}".format(name))
-    if not RegisterModel.find_by_name(name):
-        url = f"http://www.{name}.com"
-        try:
-            resp = get(url=url)
-            if resp.status_code == 200:
-                print(f"{url} - ok")
-                RegisterModel(name=name, url=url).save_to_db()
-        except:
-            pass
+def _scanner(name, th):
+    print("running {} for {}".format(name, th))
+    url = f"http://www.{name}.com"
+    try:
+        resp = get(url=url)
+        if resp.status_code == 200:
+            print(f"{url} - ok")
+            RegisterModel(name=name, url=url).save_to_db()
+    except:
+        pass
 
 
 class RegisterModel(db.Model):
     __tablename__ = "urls"
-
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
     url = db.Column(db.String(100), unique=True, nullable=False)
@@ -81,6 +45,10 @@ class RegisterModel(db.Model):
 
     @classmethod
     def get_urls(cls) -> List["RegisterModel"]:
+        return cls.query.order_by(text("id DESC")).all()
+
+    @classmethod
+    def get_names(cls) -> List["RegisterModel"]:
         return cls.query.all()
 
     def save_to_db(self):
@@ -102,23 +70,34 @@ def create_tables():
 
 @app.route('/scan')
 def scan_urls():
-
     for name in names:
-        if name[0] in "abcde":
-            Thread(target=_scanner_1(name), args=[]).start()
-        if name[0] in "fghij":
-            Thread(target=_scanner_2(name), args=[]).start()
-        if name[0] in "klmnop":
-            Thread(target=_scanner_3(name), args=[]).start()
-        if name[0] in "qrstuvwxyz":
-            Thread(target=_scanner_4(name), args=[]).start()
+        if not RegisterModel.find_by_name(name):
+            if name[0] in "abcde":
+                Thread(target=_scanner(name, "th1"), args=[]).start()
+            if name[0] in "fghij":
+                Thread(target=_scanner(name, "th2"), args=[]).start()
+            if name[0] in "klmnop":
+                Thread(target=_scanner(name, "th3"), args=[]).start()
+            if name[0] in "qrstuvwxyz":
+                Thread(target=_scanner(name, "th4"), args=[]).start()
 
-    return redirect(url_for('get_urls'))
-
+    return redirect(url_for('clean'))
 
 @app.route('/urls')
 def get_urls():
     return {"urls": [r.to_json() for r in RegisterModel.get_urls()]}, 200
+
+@app.route('/<string:name>')
+def get_url(name):
+    if (result := RegisterModel.find_by_name(name)) is None:
+        return {"message": f"{name} not found."}
+    return {"url": result.to_json()}, 200
+
+@app.route('/clean')
+def clean():
+    clean_names(set([r.to_json()['name'] for r in RegisterModel.get_names()]), names)
+    return redirect(url_for('get_urls'))
+
 
 db.init_app(app)
 
--- names.py
+++ names.py
@@ -1,5 +1,24 @@
+import os
+
 names = set()
 
 with open("names.txt", mode="r") as f:
-    for name in f.readlines():
+    readded = f.readlines()
+    for name in readded:
         names.add(name.split('\n')[0])
+    print("There are {} names into file".format(len(names)))
+
+def clean_names(new, old):
+    global names
+    added = old.difference(new)
+    names = added
+    added = '\n'.join(added)
+
+    with open("names_new.txt", mode="w+") as file:
+        file.writelines(added)
+
+    print("There are {} names into new file".format(len(names)))
+    os.remove("names.txt")
+    os.rename("names_new.txt", "names.txt")
+
+
